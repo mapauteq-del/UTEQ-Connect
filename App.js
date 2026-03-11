334319b299497d09adab1e33c4a36023a2d32a65
morgan24jm <qdj15hj@gmail.com>
2026-03-11 15:10:15 -0600

    Creacion de pantalla Perfil y Agregacion de navbar inferior

--- App.js
+++ App.js
@@ -4,7 +4,8 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Index from './src/screens/Index';
 import LoginScreen from './src/screens/LoginScreen';
 import RegisterScreen from './src/screens/RegisterScreen';
-import MapScreen from './src/Views/MapScreen';
+import MapScreen from './src/screens/MapScreen';
+import ProfileScreen from './src/screens/ProfileScreen';
 
 const Stack = createStackNavigator();
 
@@ -22,6 +23,7 @@ export default function App() {
         <Stack.Screen name="Login" component={LoginScreen} />
         <Stack.Screen name="Register" component={RegisterScreen} />
         <Stack.Screen name="Map" component={MapScreen} />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
